b247bf3d48828a8152fd8378f6788b6db97a9664
RICH25-LAB <richardmateleka10@gmail.com>
2026-03-01 19:00:10 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>NISAIDIE NISOME INITIATIVE</h1>
+      <p>Providing learning materials to children in rural areas.</p>
+      <p>Website is live.</p>
+    </div>
+  );
+}
