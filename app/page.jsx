90fde02685dc84e9bd4f0b3158bfdc3edb63ff50
RICH25-LAB <richardmateleka10@gmail.com>
2026-03-01 18:34:07 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>NISAIDIE NISOME INITIATIVE</h1>
+      <p>Providing learning materials to children in rural areas.</p>
+      <p>Website is live.</p>
+    </main>
+  );
+}
