c22fe13c43274162f1ef357479bf9ebe6e30a1ea
RICH25-LAB <richardmateleka10@gmail.com>
2026-03-01 19:04:55 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1 style={{ color: "green" }}>NISAIDIE NISOME INITIATIVE</h1>
+      <p>Providing learning materials to children in rural areas.</p>
+      <p>Website is live.</p>
+    </div>
+  );
+}
